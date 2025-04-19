b79e2e7e37438b0291ecf7762cbdec833ee10c6b
foksi71 <reshetkoandrii18@gmail.com>
2025-04-19 12:19:44 +0300

    Up9sl main.py

--- main.py
+++ main.py
@@ -134,8 +134,8 @@ KEYWORDS = [
 
 # Отримувачі (username або ID)
 TARGET_ACCOUNTS = [
-    'andryy_99', 'dim_gtr', 'maxx_py', 'kentvr01', 'romixsi', 'k_valer1', 'karpatyu', 'karnazh01', #'neksiix'
-]
+    'andryy_99', 'dim_gtr', 'maxx_py', 'kentvr01', 'romixsi', 'k_valer1', 'karpatyu', 'karnazh01',
+]  #'neksiix'
 
 # Ініціалізація Telegram клієнта
 client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
